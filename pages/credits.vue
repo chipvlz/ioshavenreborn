49f46a0d6ebaff4dc46f30e04a503ddf32edc4d2
Unknown <zackbaldwin148@gmail.com>
2017-10-27 18:35:28 -0400

    Hopefully final revision of Credits Page

--- pages/credits.vue
+++ pages/credits.vue
@@ -4,10 +4,10 @@
     <div class="heading">
         iOS Haven Staff
     </div>
-      <ul class="fancy">
-        <li>Zeb - Developer<a href="https://twitter.com/gfxrrr" target="_blank">Twitter</a></li>
-        <li>Zack - Developer<a href="https://twitter.com/_ZackBz" target="_blank">Twitter</a></li>
-        <li>Zane - Devleoper<a href="https://twitter.com/ZaneDevs" target="_blank">twitter</a></li>
+      <ul class="fancy bigger">
+        <li>Zeb - Backend &amp; Frontend Developer<a href="https://twitter.com/gfxrrr" target="_blank"><i class="fab fa-twitter"></i></a></li>
+        <li>Zack - Backend &amp; IPA Developer<a href="https://twitter.com/_ZackBz" target="_blank"><i class="fab fa-twitter"></i></a></li>
+        <li>Zane - IPA Devleoper<a href="https://twitter.com/ZaneDevs" target="_blank"><i class="fab fa-twitter"></i></a></li>
       </ul>
   </div>
 
@@ -15,20 +15,20 @@
     <div class="heading">
       App Providers
     </div>
-    <ul class="fancy">
-      <li>UnlimApps | ++ Tweaks<a href="https://twitter.com/unlimapps" target="_blank">Twitter</a></li>
-      <li>Majd Alfhaily | Instagram Rocket &amp; Cercube<a href="https://twitter.com/freemanrepo" target="_blank">Twitter</a></li>
-      <li>CokePokes | Phantom for Snapcaht<a href="https://twitter.com/CokePokes" target="_blank">Twitter</a></li>
+    <ul class="fancy bigger">
+      <li>UnlimApps | ++ Tweaks<a href="https://twitter.com/unlimapps" target="_blank"><i class="fab fa-twitter"></i></a></li>
+      <li>Majd Alfhaily | Instagram Rocket &amp; Cercube<a href="https://twitter.com/freemanrepo" target="_blank"><i class="fab fa-twitter"></i></a></li>
+      <li>CokePokes | Phantom for Snapchat<a href="https://twitter.com/CokePokes" target="_blank"><i class="fab fa-twitter"></i></a></li>
       <li>Othman Alomiry | Snapchat SCOthman</li>
-      <li>Pokego++ | Pokego++<a href="https://www.pokego2.com/" target="_blank">PokeGo++ Website</a></li>
-      <li>Bobby Movie &amp; Bobby Music<a href="https://twitter.com/bobbymovie" target="_blank">Twitter</a></li>
-      <li>Playbox | CinemaBox PB<a href="https://twitter.com/playbox_hd" target="_blank">Twitter</a></li>
-      <li>Popcorn Time | Popcorn Time<a href="https://popcorn-time.to/" target="_blank">Twitter</a> </li>
-      <li>EveryCord | EveryCord<a href="https://twitter.com/everycord" target="_blank">Twitter</a></li>
-      <li>F.lux | F.lux<a href="https://justgetflux.com/" target="_blank">Twitter</a></li>
-      <li>Ivano Bilenchi | iCleaner<a href="https://ib-soft.net/" target="_blank">Twitter</a></li>
-      <li>iDarkmode | iDarkmode<a href="https://twitter.com/idarkmode" target="_blank">Twitter</a></li>
-      <li>MasonD3V | iTD App<a href="https://twitter.com/MasonD3V" target="_blank">Twitter</a></li>
+      <li>Pokego++ | Pokego++<a href="https://twitter.com/OfficialPokeGo2?lang=en" target="_blank"><i class="fab fa-twitter"></i></a></li>
+      <li>Bobby Movie &amp; Bobby Music<a href="https://twitter.com/bobbymovie" target="_blank"><i class="fab fa-twitter"></i></a></li>
+      <li>Playbox | CinemaBox PB<a href="https://twitter.com/playbox_hd" target="_blank"><i class="fab fa-twitter"></i></a></li>
+      <li>Popcorn Time | Popcorn Time<a href="https://popcorn-time.to/" target="_blank"><i class="fab fa-twitter"></i></a> </li>
+      <li>EveryCord | EveryCord<a href="https://twitter.com/everycord" target="_blank"><i class="fab fa-twitter"></i></a></li>
+      <li>F.lux | F.lux<a href="https://justgetflux.com/" target="_blank"><i class="fab fa-twitter"></i></a></li>
+      <li>Ivano Bilenchi | iCleaner<a href="https://ib-soft.net/" target="_blank"><i class="fab fa-twitter"></i></a></li>
+      <li>iDarkmode | iDarkmode<a href="https://twitter.com/idarkmode" target="_blank"><i class="fab fa-twitter"></i></a></li>
+      <li>MasonD3V | iTD App<a href="https://twitter.com/MasonD3V" target="_blank"><i class="fab fa-twitter"></i></a></li>
     </ul>
   </div>
 
@@ -37,11 +37,11 @@
       App Providers
     </div>
     <div>
-      <ul class="fancy">
-        <li>Emus4<a href="https://twitter.com/Emus4you" target="_blank">Twitter</a></li>
-        <li>IraqStore<a href="https://ir4q.store" target="_blank">Twitter</a></li>
-        <li>AppValley<a href="https://twitter.com/appvalley_vip" target="_blank">Twitter</a></li>
-        <li>Hassan Abdullah<a href="https://twitter.com/kakaswr22" target="_blank">Twitter</a></li>
+      <ul class="fancy bigger">
+        <li>Emus4<a href="https://twitter.com/Emus4you" target="_blank"><i class="fab fa-twitter"></i></a></li>
+        <li>IraqStore<a href="https://ir4q.store" target="_blank"><i class="fab fa-twitter"></i></a></li>
+        <li>AppValley<a href="https://twitter.com/appvalley_vip" target="_blank"><i class="fab fa-twitter"></i></a></li>
+        <li>Hassan Abdullah<a href="https://twitter.com/kakaswr22" target="_blank"><i class="fab fa-twitter"></i></a></li>
       </ul>
     </div>
   </div>
@@ -51,8 +51,8 @@
       Jailbreak Developers
     </div>
     <div>
-      <ul class="fancy">
-        <li>qwertyoruiopz | yalu102<a href="https://twitter.com/qwertyoruiopz" target="_blank">Twitter</a></li>
+      <ul class="fancy bigger">
+        <li>qwertyoruiopz | yalu102 <a href="https://twitter.com/qwertyoruiopz" target="_blank"><i class="fab fa-twitter"></i></a></li>
         <li>Ian Beer | extra_recipe</li>
       </ul>
     </div>
