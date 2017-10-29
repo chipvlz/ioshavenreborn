88658bebef1c6953bf2ebadfa6e2436c97b303b3
Unknown <zackbaldwin148@gmail.com>
2017-10-28 23:56:11 -0400

    favicons

--- build/main.js
+++ build/main.js
@@ -98,7 +98,7 @@ app.use('/api', __WEBPACK_IMPORTED_MODULE_2__api__["a" /* default */]);
 
 // Import and Set Nuxt.js options
 var config = __webpack_require__(5);
-config.dev = !("production" === 'production');
+config.dev = !("development" === 'production');
 
 // Init Nuxt.js
 var nuxt = new __WEBPACK_IMPORTED_MODULE_1_nuxt__["Nuxt"](config);
@@ -181,9 +181,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
-    meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }, { hid: 'description', name: 'description', content: 'Nuxt.js project' }],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, { rel: 'stylesheet', href: '/fa/css/fontawesome-pro-brands.css' }, { rel: 'stylesheet', href: '/fa/css/fontawesome-pro-solid.css' }, { rel: 'stylesheet', href: '/fa/css/fontawesome-pro-light.css' }, { rel: 'stylesheet', href: '/fa/css/fontawesome-pro-regular.css' }, { rel: 'stylesheet', href: '/fa/css/fontawesome-pro-core.css' }, { rel: 'stylesheet', href: '/markdown.css' }],
+    title: 'iOS Haven',
+    meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }, { hid: 'description', name: 'description', content: 'Nuxt.js project' }, { name: 'apple-mobile-web-app-title', content: 'iOS Haven' }, { name: 'application-name', content: 'iOS Haven' }, { name: 'theme-color', content: '#ffffff' }],
+    link: [
+    // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    { rel: 'stylesheet', href: '/fa/css/fontawesome-pro-brands.css' }, { rel: 'stylesheet', href: '/fa/css/fontawesome-pro-solid.css' }, { rel: 'stylesheet', href: '/fa/css/fontawesome-pro-light.css' }, { rel: 'stylesheet', href: '/fa/css/fontawesome-pro-regular.css' }, { rel: 'stylesheet', href: '/fa/css/fontawesome-pro-core.css' }, { rel: 'stylesheet', href: '/markdown.css' }, { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png?v=QEMYzE9pb35555' }, { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png?v=QEMYzE9pb35555' }, { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png?v=QEMYzE9pb35555' }, { rel: 'manifest', href: '/favicons/manifest.json?v=QEMYzE9pb35555' }, { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg?v=QEMYzE9pb35555', color: '#e81e1e' }, { rel: 'shortcut icon', href: '/favicons/favicon.ico?v=QEMYzE9pb35555' }],
     script: [{ type: 'text/javascript', 'src': 'discord.min.js' }]
   },
   /*
